4c04b6cad9461591197f4dbfdcc6ea881f647b6c
Tomas Felner <tom@server.felner.com>
2016-01-03 14:40:25 +0100

    added felner customizations

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+  * -----------------------------------------------------------------------------
+  * @package     smartVISU
+  * @author      Martin Gleiß
+  * @copyright   2012
+  * @license     GPL [http://www.gnu.de]
+  * -----------------------------------------------------------------------------
+  */
+
+
+	define('config_pages', 'felner');
+	define('config_design', 'night');
+	define('config_cache', false);
+	define('config_animation', true);
+	define('config_title', 'Zwillikon');
+	define('config_lang', 'de');
+	define('config_driver', 'smarthome.py');
+	define('config_driver_address', '192.168.10.136');
+	define('config_driver_port', '2424');
+	define('config_driver_realtime', true);
+	define('config_weather_service', 'wunderground.com');
+	define('config_weather_location', 'Switzerland/Affoltern');
+	define('config_weather_key', '969b2ee414b53af4');
+	define('config_phone_service', 'offline');
+	define('config_phone_server', '192.168.x.x');
+	define('config_phone_user', '');
+	define('config_phone_pass', '');
+	define('config_calendar_service', 'offline');
+	define('config_calendar_url', 'http://www.google.com/calendar/feeds/...');
+	define('config_js', 'min.js');
+
+?>
